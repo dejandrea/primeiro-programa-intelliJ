2ff769f84d67ab04aa723343c3c0776be166ac68
Andréa J S França <dejandrea@gmail.com>
2022-07-10 01:54:03 -0300

    update

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,57 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O pequeno principe",300,"lsjflksjf");
+        System.out.println(livro1);
+
 
 //        int a = 5;
 //        int b = 3;
 //        System.out.println("Hello World!"+(a+b));
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer nPages;
+    private String author;
+
+    public Livro(String nome, Integer nPages, String author) {
+        this.nome = nome;
+        this.nPages = nPages;
+        this.author = author;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getnPages() {
+        return nPages;
+    }
+
+    public void setnPages(Integer nPages) {
+        this.nPages = nPages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", nPages=" + nPages +
+                ", author='" + author + '\'' +
+                '}';
     }
 }
